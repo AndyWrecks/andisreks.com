6b38717d318b17108dae93af3ba5e7e4a9b9df63
Andy Reks <andiskreks@gmail.com>
2022-12-12 13:00:48 -0500

    Vuetify theme update

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,37 @@ import router from "./router";
 import "@mdi/font/css/materialdesignicons.css";
 
 import "vuetify/styles";
-import { createVuetify } from "vuetify";
+import { createVuetify, ThemeDefinition } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#F7FAF9",
+    surface: "#F7FAF9",
+    primary: "#345054",
+    "primary-darken-1": "#345054",
+    secondary: "#7D8BA5",
+    "secondary-darken-1": "#7D8BA5",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: "myCustomLightTheme",
+    themes: {
+      myCustomLightTheme,
+    },
+  },
+
   icons: {
     defaultSet: "mdi",
     aliases,
