6cda1886793417399a056089eaf10cd3301bcd0c
Andis Reks <andy@commonwealthgiant.com>
2022-12-12 12:40:56 -0500

    Adding Vuetify

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     <router-link to="/about">About</router-link> |
     <router-link to="/resume">Resume</router-link>
   </nav>
+  <v-icon icon="mdi-home" />
+  git
   <router-view />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,25 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/index.css";
 import router from "./router";
+import "@mdi/font/css/materialdesignicons.css";
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 
 /* import the fontawesome core */
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -20,4 +39,5 @@ library.add(faEnvelope, faLinkedin, faLocationDot);
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
   .use(router)
+  .use(vuetify)
   .mount("#app");
