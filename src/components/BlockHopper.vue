a6c4dc9e84fec08b194f883d84fdfcc41ae85b7e
Andy Reks <andiskreks@gmail.com>
2023-01-05 11:18:51 -0500

    block-hopper start

---
+++ src/components/BlockHopper.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="canvas"></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "BlockHopper",
+  mounted() {
+    const canvasWidth = 600;
+    const canvasHeight = 400;
+
+    const gameCanvas = {
+      canvas: document.createElement("canvas"),
+      start: function () {
+        this.canvas.width = canvasWidth;
+        this.canvas.height = canvasHeight;
+
+        Object.assign(this, { context: this.canvas.getContext("2d") });
+        document.getElementById("canvas")?.appendChild(this.canvas);
+      },
+    };
+
+    gameCanvas.start();
+  },
+};
+</script>
+
+<style>
+canvas {
+  border: 2px solid black;
+  background-color: #87ceeb;
+}
+</style>
