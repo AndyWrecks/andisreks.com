53b807a7ee6892c4a84b63e6c778bdf34e6ac4fa
Andis Reks <andy@commonwealthgiant.com>
2023-01-05 12:11:49 -0500

    Adding jump functionality

--- src/components/BlockHopper.vue
+++ src/components/BlockHopper.vue
@@ -2,27 +2,105 @@
   <div id="canvas"></div>
 </template>
 
-<script lang="ts">
-export default {
-  name: "BlockHopper",
-  mounted() {
-    const canvasWidth = 600;
-    const canvasHeight = 400;
+<script>
+import { defineComponent } from "vue";
 
-    const gameCanvas = {
-      canvas: document.createElement("canvas"),
-      start: function () {
-        this.canvas.width = canvasWidth;
-        this.canvas.height = canvasHeight;
+const canvasWidth = 600;
+const canvasHeight = 400;
+let fallSpeed = 0;
+let isJumping = false;
+let jumpSpeed = 0;
 
-        Object.assign(this, { context: this.canvas.getContext("2d") });
-        document.getElementById("canvas")?.appendChild(this.canvas);
-      },
-    };
+const gameCanvas = {
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = canvasWidth;
+    this.canvas.height = canvasHeight;
 
-    gameCanvas.start();
+    Object.assign(this, { context: this.canvas.getContext("2d") });
+    document.getElementById("canvas")?.appendChild(this.canvas);
   },
 };
+
+export default defineComponent({
+  name: "BlockHopper",
+  methods: {
+    initGameCanvas: () => {
+      gameCanvas.start();
+    },
+    createPlayer: function (width, height, x) {
+      const playerYPosition = 200;
+
+      this.width = width;
+      this.height = height;
+      this.x = x;
+      this.y = playerYPosition;
+
+      this.draw = function () {
+        const ctx = gameCanvas.context;
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      };
+
+      this.makeFall = function () {
+        if (!isJumping) {
+          this.y += fallSpeed;
+          fallSpeed += 0.1;
+          this.stopPlayer();
+        }
+      };
+
+      this.stopPlayer = function () {
+        const ground = canvasHeight - this.height;
+
+        if (this.y > ground) {
+          this.y = ground;
+        }
+      };
+
+      this.jump = function () {
+        if (isJumping) {
+          this.y -= jumpSpeed;
+          jumpSpeed += 0.3;
+        }
+      };
+    },
+    resetJump: function () {
+      jumpSpeed = 0;
+      isJumping = false;
+    },
+    startGame: function () {
+      this.initGameCanvas();
+      this.player = new this.createPlayer(30, 30, 10);
+      setInterval(this.updateCanvas, 20);
+      const resetJump = this.resetJump;
+
+      window.addEventListener("keydown", (event) => {
+        if (event.defaultPrevented) {
+          return;
+        }
+
+        if (event.code === "Space") {
+          isJumping = true;
+          setTimeout(function () {
+            resetJump();
+          }, 1000);
+        }
+      });
+    },
+    updateCanvas: function () {
+      const ctx = gameCanvas.context;
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+      this.player.makeFall();
+      this.player.draw();
+      this.player.jump();
+    },
+  },
+  mounted() {
+    this.startGame();
+  },
+});
 </script>
 
 <style>
