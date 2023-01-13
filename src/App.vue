597c5aa2e830c5b9d9d67f178fce2dda66a36a00
Andis Reks <andy@commonwealthgiant.com>
2023-01-13 11:58:57 -0500

    Putting Nav in drawer for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <template>
   <v-app>
     <v-layout>
-      <div class="print:hidden">
-        <v-app-bar>
-          <nav class="flex flex-row items-baseline">
-            <span class="text-primary text-xl mr-6"
-              >Andis<span class="text-secondary">Reks</span></span
-            >
-
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
-            <router-link to="/media-log">Media Log</router-link> |
-            <router-link to="/resume">Resume</router-link> |
-            <router-link to="/block-hopper">Block Hopper</router-link> |
-            <router-link to="/posts">Posts</router-link> |
-          </nav>
-        </v-app-bar>
-      </div>
-
       <!--      <v-navigation-drawer color="grey-darken-2"></v-navigation-drawer>-->
-
+      <AppNav />
       <v-main class="print:pt-0">
         <router-view />
       </v-main>
@@ -41,3 +24,10 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script>
+import AppNav from "@/AppNav";
+
+export default {
+  components: { AppNav },
+};
+</script>
---
+++ src/AppNav.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="print:hidden">
+    <v-navigation-drawer
+      :v-click-outside="toggleNav"
+      temporary
+      v-model="navOpen"
+    >
+      <v-list>
+        <v-list-item><router-link to="/">Home</router-link></v-list-item>
+
+        <v-list-item><router-link to="/about">About</router-link></v-list-item>
+
+        <v-list-item
+          ><router-link to="/media-log">Media Log</router-link></v-list-item
+        >
+
+        <v-list-item
+          ><router-link to="/resume">Resume</router-link></v-list-item
+        >
+
+        <v-list-item
+          ><router-link to="/block-hopper"
+            >Block Hopper</router-link
+          ></v-list-item
+        >
+
+        <v-list-item><router-link to="/posts">Posts</router-link></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon
+          class="md:hidden"
+          v-on:click="toggleNav"
+        ></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title class="text-primary text-xl mr-6"
+        >Andis<span class="text-secondary">Reks</span>
+      </v-app-bar-title>
+    </v-app-bar>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "AppNav",
+  data() {
+    return {
+      navOpen: false,
+    };
+  },
+  computed: {
+    drawerOpen() {
+      const store = useStore();
+
+      console.log(store.getters["mediaLog/getNavStatus"]);
+      return store.getters["mediaLog/getNavStatus"];
+    },
+  },
+  methods: {
+    toggleNav() {
+      this.navOpen = !this.navOpen;
+    },
+  },
+});
+</script>
+<style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+</style>
